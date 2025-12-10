01a4c0ced30d0aaff80d9558e83b481632062b5c
CobaltDream <3567840474@qq.com>
2025-12-10 15:12:38 +0800

    replace software with uadk

--- vcrypto.sh
+++ vcrypto.sh
@@ -1,2 +1,10 @@
-sudo rm /tmp/vhost_crypto.sock
-sudo ./build/examples/dpdk-vhost_crypto -l 1-3 -n 4 --vdev 'crypto_openssl' -- --socket-file 2,/tmp/vhost_crypto.sock
+sudo \
+WD_RSA_CTX_NUM="sync:2@0,async:4@0" \
+WD_DH_CTX_NUM="sync:2@0,async:4@0" \
+WD_CIPHER_CTX_NUM="sync:2@2,async:4@2" \
+WD_DIGEST_CTX_NUM="sync:2@2,async:4@2" \
+bash -c '
+rm -f /tmp/vhost_crypto.sock
+./build/examples/dpdk-vhost_crypto -l 1-3 -n 4 --vdev "crypto_uadk" -- --socket-file 2,/tmp/vhost_crypto.sock --zero-copy
+'
+
--- vhost.sh
+++ vhost.sh
@@ -1,4 +1,10 @@
-sudo rm /tmp/vhost_compress.sock
+sudo \
+WD_RSA_CTX_NUM="sync:2@0,async:4@0" \
+WD_DH_CTX_NUM="sync:2@0,async:4@0" \
+WD_CIPHER_CTX_NUM="sync:2@2,async:4@2" \
+WD_DIGEST_CTX_NUM="sync:2@2,async:4@2" \
+WD_COMP_CTX_NUM="sync_comp:2@2,sync_decomp:2@2,async_comp:4@2,async_decomp:4@2" \
+bash -c 'rm /tmp/vhost_compress.sock
 sudo ./build/examples/dpdk-vhost_compress \
 --socket-mem 1024,0 -n 4 --vdev="compress_zlib" \
--- --socket-file 2,/tmp/vhost_compress.sock --config=0,0,0
+-- --socket-file 2,/tmp/vhost_compress.sock --config=0,0,0'
